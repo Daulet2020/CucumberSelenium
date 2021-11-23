64d6815b9090d9895e781cd7846a2d8dfb49a28b
daule <daulet2020@gmail.com>
2021-11-23 15:14:52 -0600

    new

--- src/test/java/com/vytrack/step_definitions/Hook.java
+++ src/test/java/com/vytrack/step_definitions/Hook.java
@@ -4,6 +4,8 @@ import com.vytrack.utilities.Driver;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hook {
 
@@ -17,6 +19,8 @@ public class Hook {
     public void teardown(Scenario scenario){
         if(scenario.isFailed()){
             System.out.println("Test failed");
+            byte[] screnshot = ((TakesScreenshot)Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screnshot, "image/png");
         } else {
             System.out.println("Test completed!");
             System.out.println("################################");
